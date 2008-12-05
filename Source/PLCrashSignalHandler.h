10ed70acf70e73b584f69b884a9bb89dfd317247
Landon Fuller <landonf@plausible.coop>
2008-12-05 11:36:44 +0000

    Empty class for the signal handler implementation

---
+++ Source/PLCrashSignalHandler.h
@@ -0,0 +1,14 @@
+/*
+ * Author: Landon Fuller <landonf@plausiblelabs.com>
+ *
+ * Copyright (c) 2008 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface PLCrashSignalHandler : NSObject {
+
+}
+
+@end
---
+++ Source/PLCrashSignalHandler.m
@@ -0,0 +1,13 @@
+/*
+ * Author: Landon Fuller <landonf@plausiblelabs.com>
+ *
+ * Copyright (c) 2008 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ */
+
+#import "PLCrashSignalHandler.h"
+
+
+@implementation PLCrashSignalHandler
+
+@end
