17d9801b934c9d636a0a6c5c9233543c471b59b5
landonf@OFFICE.PLAUSIBLELABS.COM <landonf@OFFICE.PLAUSIBLELABS.COM@25172300-ee46-11dd-abe2-393a09110dd0>
2008-12-05 10:34:37 +0000

    Basic framework
    git-svn-id: https://plcrashreporter.googlecode.com/svn/trunk@2 25172300-ee46-11dd-abe2-393a09110dd0

---
+++ Source/CrashReporter.m
@@ -0,0 +1,13 @@
+/*
+ * Author: Landon Fuller <landonf@plausiblelabs.com>
+ *
+ * Copyright (c) 2008 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ */
+
+#import "CrashReporter.h"
+
+
+@implementation CrashReporter
+
+@end
