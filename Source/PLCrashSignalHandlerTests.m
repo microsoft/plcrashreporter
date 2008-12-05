b2b79408a7be99f13259265e091f6da532fd9f76
landonf@OFFICE.PLAUSIBLELABS.COM <landonf@OFFICE.PLAUSIBLELABS.COM@25172300-ee46-11dd-abe2-393a09110dd0>
2008-12-05 11:36:44 +0000

    Empty class for the signal handler implementation
    git-svn-id: https://plcrashreporter.googlecode.com/svn/trunk@14 25172300-ee46-11dd-abe2-393a09110dd0

---
+++ Source/PLCrashSignalHandlerTests.m
@@ -0,0 +1,17 @@
+/*
+ * Author: Landon Fuller <landonf@plausiblelabs.com>
+ *
+ * Copyright (c) 2008 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ */
+
+#import "GTMSenTestCase.h"
+
+@interface PLCrashSignalHandlerTests : SenTestCase @end
+
+@implementation PLCrashSignalHandlerTests
+
+- (void) testSomething {
+}
+
+@end
