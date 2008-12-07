bd14f6f42607bbd1f2f51f11cc86adc50f1944b6
landonf@OFFICE.PLAUSIBLELABS.COM <landonf@OFFICE.PLAUSIBLELABS.COM@25172300-ee46-11dd-abe2-393a09110dd0>
2008-12-07 02:16:52 +0000

    Implement signal handler registration
    git-svn-id: https://plcrashreporter.googlecode.com/svn/trunk@17 25172300-ee46-11dd-abe2-393a09110dd0

--- Source/PLCrashSignalHandlerTests.m
+++ Source/PLCrashSignalHandlerTests.m
@@ -17,4 +17,20 @@
     STAssertNotNil([PLCrashSignalHandler sharedHandler], @"Nil shared handler");
 }
 
+- (void) testRegisterSignalHandlers {
+    NSError *error;
+    struct sigaction action;
+
+    /* Ensure that no signal handler is registered for one of the fatal signals */
+    sigaction (SIGSEGV, NULL, &action);
+    STAssertEquals(action.sa_handler, SIG_DFL, @"Action already registered for SIGSEGV");
+
+    /* Register the signal handlers */
+    STAssertTrue([[PLCrashSignalHandler sharedHandler] registerHandlerAndReturnError: &error], @"Could not register signal handler: %@", error);
+
+    /* Check for SIGSEGV registration */
+    sigaction (SIGSEGV, NULL, &action);
+    STAssertNotEquals(action.sa_handler, SIG_DFL, @"Action not registered for SIGSEGV");
+}
+
 @end
