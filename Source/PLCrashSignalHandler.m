4fda5cd80d16b68fecb44a00cd2538ed1d8edcf9
landonf@OFFICE.PLAUSIBLELABS.COM <landonf@OFFICE.PLAUSIBLELABS.COM@25172300-ee46-11dd-abe2-393a09110dd0>
2008-12-07 22:36:59 +0000

    - Move common definitions out of the framework-wide include file, and into an include each source file can include seperately -- that way we don't have to rebuild the framework each time a header is built. - Add support for spawning the error handling thread. - Wire up error handling for all the possible mach and posix errors that occur.
    git-svn-id: https://plcrashreporter.googlecode.com/svn/trunk@24 25172300-ee46-11dd-abe2-393a09110dd0

--- Source/PLCrashSignalHandler.m
+++ Source/PLCrashSignalHandler.m
@@ -5,59 +5,47 @@
  * All rights reserved.
  */
 
-#import <signal.h>
-#import <ucontext.h>
-#include <unistd.h>
+#import <pthread.h>
 
+#import <mach/mach.h>
+
+#import "PLCrashReporter.h"
 #import "PLCrashSignalHandler.h"
 
 // Crash log handler
+#if 0
 static void dump_crash_log (int signal, siginfo_t *info, ucontext_t *uap);
+#endif
 
-/**
- * @internal
- * List of monitored fatal signals.
- */
-static int fatal_signals[] = {
-    SIGABRT,
-    SIGBUS,
-    SIGFPE,
-    SIGILL,
-    SIGSEGV
-};
+@interface PLCrashSignalHandler (PrivateMethods)
+
+- (void) populateError: (NSError **) error
+              errnoVal: (int) errnoVal
+           description: (NSString *) description;
+
+- (void) populateError: (NSError **) error
+             machError: (kern_return_t) machError
+           description: (NSString *) description;
+
+- (void) populateError: (NSError **) error 
+             errorCode: (PLCrashReporterError) code
+           description: (NSString *) description
+                 cause: (NSError *) cause;
+
+@end
 
 /** @internal
- * number of signals in the fatal signals list */
-static int n_fatal_signals = (sizeof(fatal_signals) / sizeof(fatal_signals[0]));
-
-
-/** @internal
- * Root fatal signal handler */
-static void fatal_signal_handler (int signal, siginfo_t *info, void *uapVoid) {
-    /* Remove all signal handlers -- if the dump code fails, the default terminate
-     * action will occur */
-    for (int i = 0; i < n_fatal_signals; i++) {
-        struct sigaction sa;
-        
-        memset(&sa, 0, sizeof(sa));
-        sa.sa_handler = SIG_DFL;
-        sigemptyset(&sa.sa_mask);
-        
-        sigaction(fatal_signals[i], &sa, NULL);
-    }
-
-    /* Call the crash log dumper */
-    dump_crash_log(signal, info, uapVoid);
-
-    /* Re-raise the signal */
-    raise(signal);
-}
-
-/**
- * @internal
- * Shared signal handler. Only one may be allocated per process, for obvious reasons.
- */
+ * Shared signal handler. Only one may be allocated per process, for obvious reasons. */
 static PLCrashSignalHandler *sharedHandler;
+
+/**
+ * @internal
+ * Background thread that receives and forwards Mach exception handler messages
+ */
+static void *exception_handler_thread (void *arg) {
+    
+    return NULL;
+}
 
 /***
  * @internal
@@ -65,10 +53,12 @@ static PLCrashSignalHandler *sharedHandler;
  */
 @implementation PLCrashSignalHandler
 
+
 /* Set up the singleton signal handler */
 + (void) initialize {
     sharedHandler = [[PLCrashSignalHandler alloc] init];
 }
+
 
 /**
  * Return the process signal handler. Only one signal handler may be registered per process.
@@ -80,42 +70,10 @@ static PLCrashSignalHandler *sharedHandler;
     return sharedHandler;
 }
 
-/**
- * Register the the signal handler for the given signal.
- *
- * @param outError A pointer to an NSError object variable. If an error occurs, this
- * pointer will contain an error object indicating why the signal handlers could not be
- * registered. If no error occurs, this parameter will be left unmodified. You may specify
- * NULL for this parameter, and no error information will be provided. 
- */
-- (BOOL) registerHandlerForSignal: (int) signal error: (NSError **) outError {
-    struct sigaction sa;
 
-    /* Configure action */
-    memset(&sa, 0, sizeof(sa));
-    sa.sa_flags = SA_SIGINFO;
-    sigemptyset(&sa.sa_mask);
-    sa.sa_sigaction = &fatal_signal_handler;
-
-    /* Set new sigaction */
-    if (sigaction(signal, &sa, NULL) != 0) {
-        int err = errno;
-        if (outError)
-            *outError = [NSError errorWithDomain: NSPOSIXErrorDomain code: errno userInfo: nil];
-
-        NSLog(@"Signal registration for %s failed: %s", strsignal(signal), strerror(err));
-        return NO;
-    }
-
-    return YES;
-}
 
 /**
- * Register the process signal handlers. May be called more than once, but will
- * simply reset previously registered signal handlers.
- *
- * @note If this method returns NO, some signal handlers may have been registered
- * successfully.
+ * Register the Mach exception handlers.
  *
  * @param outError A pointer to an NSError object variable. If an error occurs, this
  * pointer will contain an error object indicating why the signal handlers could not be
@@ -123,13 +81,50 @@ static PLCrashSignalHandler *sharedHandler;
  * NULL for this parameter, and no error information will be provided. 
  */
 - (BOOL) registerHandlerAndReturnError: (NSError **) outError {
-    /* Register handler for signals */
-    for (int i = 0; i < n_fatal_signals; i++) {
-        if (![self registerHandlerForSignal: fatal_signals[i] error: outError])
-            return NO;
+    static BOOL initialized = NO;
+    pthread_attr_t attr;
+    pthread_t thr;
+    
+    /* Must only be called once per process */
+    if (initialized) {
+        [NSException raise: PLCrashReporterException format: @"Attempted to re-register per-process signal handler"];
+        return NO;
+    }
+    
+    /* Set up the exception handler thread. The thread should be extremely lightweight, as it only
+     * exists to handle very unusual conditions.
+     *
+     * We use a very small stack size (4k or PTHREAD_STACK_MIN, whichever is larger) */
+    if (pthread_attr_init(&attr) != 0) {
+        [self populateError: outError errnoVal: errno description: NSLocalizedString(@"Could not set pthread stack size", @"")];
+        return NO;
     }
 
-    return YES;
+    if (pthread_attr_setstacksize(&attr, MAX(PTHREAD_STACK_MIN, 4096)) != 0) {
+        [self populateError: outError errnoVal: errno description: NSLocalizedString(@"Could not set pthread stack size", @"")];
+        goto cleanup;
+    }
+
+    /* start the thread detached */
+    if (pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED) != 0) {
+        [self populateError: outError errnoVal: errno description: NSLocalizedString(@"Could not set pthread detach state", @"")];
+        goto cleanup;
+    }
+    
+    /* Create the exception handler thread */
+    if (pthread_create(&thr, &attr, exception_handler_thread, NULL) != 0) {
+        [self populateError: outError errnoVal: errno description: NSLocalizedString(@"Could not create exception handler thread", @"")];
+        goto cleanup;
+    }
+    
+    /* Success, set initialized */
+    initialized = YES;
+    
+cleanup:
+    /* Clean up */
+    pthread_attr_destroy(&attr);
+    
+    return initialized;
 }
 
 /**
@@ -141,26 +136,71 @@ static PLCrashSignalHandler *sharedHandler;
  * @param address Fault address.
  */
 - (void) testHandlerWithSignal: (int) signal code: (int) code faultAddress: (void *) address {
-    siginfo_t info;
-    ucontext_t uap;
 
-    /* Initialze a faux-siginfo instance */
-    info.si_addr = address;
-    info.si_errno = 0;
-    info.si_pid = getpid();
-    info.si_uid = getuid();
-    info.si_code = code;
-    info.si_status = signal;
-    
-    /* Get the current thread's ucontext */
-    getcontext(&uap);
-
-    /* Execute the crash log handler */
-    dump_crash_log(signal, &info, &uap);
 }
 
 @end
 
+
+@implementation PLCrashSignalHandler (PrivateMethods)
+
+/**
+ * Populate an PLCrashReporterErrorOperatingSystem NSError instance, using the provided
+ * errno error value to create the underlying error cause.
+ */
+- (void) populateError: (NSError **) error
+              errnoVal: (int) errnoVal
+           description: (NSString *) description
+{
+    NSError *cause = [NSError errorWithDomain: NSPOSIXErrorDomain code: errnoVal userInfo: nil];
+    [self populateError: error errorCode: PLCrashReporterErrorOperatingSystem description: description cause: cause];
+}
+
+/**
+ * Populate an PLCrashReporterErrorOperatingSystem NSError instance, using the provided
+ * mach error value to create the underlying error cause.
+ */
+- (void) populateError: (NSError **) error
+             machError: (kern_return_t) machError
+           description: (NSString *) description
+{
+    NSError *cause = [NSError errorWithDomain: NSMachErrorDomain code: machError userInfo: nil];
+    [self populateError: error errorCode: PLCrashReporterErrorOperatingSystem description: description cause: cause];
+}
+
+/**
+ * Populate an NSError instance with the provided information.
+ *
+ * @param error Error instance to populate. If NULL, this method returns
+ * and nothing is modified.
+ * @param code The error code corresponding to this error.
+ * @param description A localized error description.
+ * @param cause The underlying cause, if any. May be nil.
+ */
+- (void) populateError: (NSError **) error 
+             errorCode: (PLCrashReporterError) code
+           description: (NSString *) description
+                 cause: (NSError *) cause
+{
+    NSMutableDictionary *userInfo;
+    
+    if (error == NULL)
+        return;
+
+    /* Create the userInfo dictionary */
+    userInfo = [NSMutableDictionary dictionaryWithObjectsAndKeys:
+                description, NSLocalizedDescriptionKey,
+                nil
+                ];
+
+    /* Add the cause, if available */
+    if (cause != nil)
+        [userInfo setObject: cause forKey: NSUnderlyingErrorKey];
+    
+    *error = [NSError errorWithDomain: PLCrashReporterErrorDomain code: code userInfo: userInfo];
+}
+
+@end
 
 /**
  * Dump the process crash log.
@@ -168,8 +208,10 @@ static PLCrashSignalHandler *sharedHandler;
  * XXX Currently outputs to stderr using NSLog, must use the crash log writer
  * implementation (once available)
  */
+#if 0
 static void dump_crash_log (int signal, siginfo_t *info, ucontext_t *uap) {
     /* Basic signal information */
     NSLog(@"Received signal %d (sig%s) code %d", signal, sys_signame[signal], info->si_code);
     NSLog(@"Signal fault address %p", info->si_addr);    
 }
+#endif
