f198cae0c2b8315a3e260862d605a4f34d4d2da4
Landon Fuller <landonf@plausible.coop>
2008-12-09 08:23:16 +0000

    Add basic ARM support

--- Source/PLCrashFrameWalker_arm.c
+++ Source/PLCrashFrameWalker_arm.c
@@ -11,21 +11,59 @@
 
 #ifdef __arm__
 
+// PLFrameWalker API
 plframe_error_t plframe_cursor_init (plframe_cursor_t *cursor, ucontext_t *uap) {
-    return PLFRAME_ENOTSUP;
+    cursor->uap = uap;
+    cursor->init_frame = true;
+    cursor->fp = NULL;
+    
+    return PLFRAME_ESUCCESS;
 }
 
 
+// PLFrameWalker API
 plframe_error_t plframe_cursor_next (plframe_cursor_t *cursor) {
-    return PLFRAME_ENOTSUP;
+    /* Fetch the next stack address */
+    if (cursor->fp == NULL) {
+        cursor->fp = (void **) cursor->uap->uc_mcontext->__ss.__r[7]; // Apple uses r7 as the frame pointer register
+    } else {
+        cursor->init_frame = false;
+        cursor->fp = cursor->fp[0];
+    }
+    
+    /* Check for completion */
+    if (cursor->fp == NULL)
+        return PLFRAME_ENOFRAME;
+    
+    /* Check for a valid address */
+    if (!plframe_valid_stackaddr(cursor->uap, cursor->fp)) {
+        return PLFRAME_EBADFRAME;
+    }
+    
+    /* New frame fetched */
+    return PLFRAME_ESUCCESS;
 }
 
 
+// PLFrameWalker API
 plframe_error_t plframe_get_reg (plframe_cursor_t *cursor, plframe_regnum_t regnum, plframe_word_t *reg) {
+    ucontext_t *uap = cursor->uap;
+    
+    /* Supported register for this context state? */
+    if (cursor->init_frame) {
+        if (regnum == PLFRAME_ARM_PC) {
+            *reg = uap->uc_mcontext->__ss.__pc;
+            return PLFRAME_ESUCCESS;
+        }
+        
+        return PLFRAME_ENOTSUP;
+    }
+
     return PLFRAME_ENOTSUP;
 }
 
 
+// PLFrameWalker API
 plframe_error_t plframe_get_freg (plframe_cursor_t *cursor, plframe_regnum_t regnum, plframe_fpreg_t *fpreg) {
     return PLFRAME_ENOTSUP;
 }
--- Source/PLCrashFrameWalker_arm.h
+++ Source/PLCrashFrameWalker_arm.h
@@ -16,13 +16,39 @@ typedef int32_t plframe_pdef_fpreg_t;
  * Arm registers
  */
 typedef enum {
+    /*
+     * General
+     */
+
+    PLFRAME_ARM_R0 = 0,
+    PLFRAME_ARM_R1,
+    PLFRAME_ARM_R2,
+    PLFRAME_ARM_R3,
+    PLFRAME_ARM_R4,
+    PLFRAME_ARM_R5,
+    PLFRAME_ARM_R6,
+    PLFRAME_ARM_R7,
+    PLFRAME_ARM_R8,
+    PLFRAME_ARM_R9,
+    PLFRAME_ARM_R10,
+    PLFRAME_ARM_R11,
+    PLFRAME_ARM_R12,
+
+    /* stack pointer (r13) */
+    PLFRAME_ARM_SP,
+
+    /* link register (r14) */
+    PLFRAME_ARM_LR,
+
+    /** Program counter (r15) */
+    PLFRAME_ARM_PC,
 
     /* Common registers */
     
-    PLFRAME_PDEF_REG_IP = 0,
+    PLFRAME_PDEF_REG_IP = PLFRAME_ARM_PC,
     
     /** Last register */
-    PLFRAME_PDEF_LAST_REG = 0
+    PLFRAME_PDEF_LAST_REG = PLFRAME_ARM_PC
 } plframe_x86_regnum_t;
 
 #endif
