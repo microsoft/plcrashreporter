4c5872512a197522b897eac42acf2c481ae88ad8
landonf@OFFICE.PLAUSIBLELABS.COM <landonf@OFFICE.PLAUSIBLELABS.COM@25172300-ee46-11dd-abe2-393a09110dd0>
2008-12-10 00:05:58 +0000

    - Add an additional test to verify that plframe_read_addr() returns an error if a bad page is read.
    git-svn-id: https://plcrashreporter.googlecode.com/svn/trunk@63 25172300-ee46-11dd-abe2-393a09110dd0

--- Source/PLCrashFrameWalkerTests.m
+++ Source/PLCrashFrameWalkerTests.m
@@ -75,8 +75,12 @@ static void stop_test_thread (pthread_t *thr, struct test_stack_args *args) {
     const char bytes[] = "Hello";
     char dest[sizeof(bytes)];
 
+    // Verify that a good read succeeds
     plframe_read_addr(bytes, dest, sizeof(dest));
     STAssertTrue(strcmp(bytes, dest) == 0, @"Read was not performed");
+    
+    // Verify that reading off the page at 0x0 fails
+    STAssertNotEquals(KERN_SUCCESS, plframe_read_addr(NULL, dest, sizeof(bytes)), @"Bad read was performed");
 }
 
 
