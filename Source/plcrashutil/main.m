5ae844fab9ea27a06802ec18ffdff056f01993fb
landonf@OFFICE.PLAUSIBLELABS.COM <landonf@OFFICE.PLAUSIBLELABS.COM@25172300-ee46-11dd-abe2-393a09110dd0>
2008-12-18 05:51:29 +0000

    - Add a static library target for Mac OS X - Add a stub plcrashutil command line tool target.
    git-svn-id: https://plcrashreporter.googlecode.com/svn/trunk@206 25172300-ee46-11dd-abe2-393a09110dd0

---
+++ Source/plcrashutil/main.m
@@ -0,0 +1,12 @@
+/*
+ * Author: Landon Fuller <landonf@plausiblelabs.com>
+ *
+ * Copyright (c) 2008 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ */
+
+#import <stdlib.h>
+
+int main (int argc, char *argv[]) {
+    exit(0);
+}
