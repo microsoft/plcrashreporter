d05f755e8149ad048aecb98dc2606ca3ddf1368d
landonf@OFFICE.PLAUSIBLELABS.COM <landonf@OFFICE.PLAUSIBLELABS.COM@25172300-ee46-11dd-abe2-393a09110dd0>
2008-12-18 01:35:33 +0000

    Add a binary image information class.
    git-svn-id: https://plcrashreporter.googlecode.com/svn/trunk@193 25172300-ee46-11dd-abe2-393a09110dd0

---
+++ Source/PLCrashLogBinaryImageInfo.h
@@ -0,0 +1,55 @@
+/*
+ * Author: Landon Fuller <landonf@plausiblelabs.com>
+ *
+ * Copyright (c) 2008 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ */
+
+#import <Foundation/Foundation.h>
+
+@interface PLCrashLogBinaryImageInfo : NSObject {
+@private
+    /** Base image address */
+    uint64_t _baseAddress;
+
+    /** Image segment size */
+    uint64_t _imageSize;
+
+    /** Name of binary image */
+    NSString *_imageName;
+
+    /** If the UUID is available */
+    BOOL _hasImageUUID;
+
+    /** 128-bit object UUID. May be nil. */
+    NSString *_imageUUID;
+}
+
+
+/**
+ * Image base address.
+ */
+@property(nonatomic, readonly) uint64_t imageBaseAddress;
+
+/**
+ * Segment size.
+ */
+@property(nonatomic, readonly) uint64_t imageSize;
+
+/**
+ * Image name (absolute path)
+ */
+@property(nonatomic, readonly) NSString *imageName;
+
+
+/**
+ * YES if this image has an associated UUID.
+ */
+@property(nonatomic, readonly) BOOL hasImageUUID;
+
+/**
+ * 128-bit object UUID (matches Mach-O DWARF dSYM files). May be nil if unavailable.
+ */
+@property(nonatomic, readonly) NSString *imageUUID;
+
+@end
