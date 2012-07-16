f96a0eb020fa89abea0fb377bdf6a7ed33c5ff68
landon.j.fuller <landon.j.fuller@25172300-ee46-11dd-abe2-393a09110dd0>
2012-07-16 19:24:08 +0000

    Fix accidental test of string pointer identity
    git-svn-id: https://plcrashreporter.googlecode.com/svn/trunk@440 25172300-ee46-11dd-abe2-393a09110dd0

--- Source/PLCrashReporterTests.m
+++ Source/PLCrashReporterTests.m
@@ -52,8 +52,8 @@
     PLCrashReport *report = [[PLCrashReport alloc] initWithData: reportData error: &error];
     STAssertNotNil(report, @"Could not parse geneated live report: %@", error);
     
-    STAssertEquals([[report signalInfo] name], @"SIGTRAP", @"Incorrect signal name");
-    STAssertEquals([[report signalInfo] code], @"TRAP_TRACE", @"Incorrect signal code");
+    STAssertEqualStrings([[report signalInfo] name], @"SIGTRAP", @"Incorrect signal name");
+    STAssertEqualStrings([[report signalInfo] code], @"TRAP_TRACE", @"Incorrect signal code");
 }
 
 @end
