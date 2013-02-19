38671ffb4f1c5c1193085c702f631c2947201020
Landon Fuller <landonf@plausible.coop>
2013-02-19 13:45:32 -0500

    Add unit test for plframe_thread_state_thread_init()

--- Source/PLCrashFrameWalkerTests.m
+++ Source/PLCrashFrameWalkerTests.m
@@ -93,6 +93,67 @@
 #endif
 }
 
+/* Test plframe_thread_state_thread_init() */
+- (void) testThreadStateThreadInit {
+    plframe_thread_state_t thr_state;
+    plframe_test_thead_t test_thr;
+    mach_msg_type_number_t state_count;
+    thread_t thr;
+
+    /* Spawn a test thread */
+    plframe_test_thread_spawn(&test_thr);
+    thr = pthread_mach_thread_np(test_thr.thread);
+    thread_suspend(thr);
+
+    /* Fetch the thread state */
+    STAssertEquals(plframe_thread_state_thread_init(&thr_state, thr), PLFRAME_ESUCCESS, @"Failed to initialize thread state");
+
+    /* Test the results */
+#if defined(PLFRAME_ARM_SUPPORT)
+    arm_thread_state_t local_thr_state;
+    state_count = ARM_THREAD_STATE_COUNT;
+
+    STAssertEquals(thread_get_state(thr, ARM_THREAD_STATE, &local_thr_state, &state_count), KERN_SUCCESS, @"Failed to fetch thread state");
+    STAssertTrue(memcmp(&thr_state.arm_state.thread, &local_thr_state, sizeof(thr_state.arm_state.thread)) == 0, @"Incorrectly copied");
+
+#elif defined(PLFRAME_X86_SUPPORT) && defined(__LP64__)
+    state_count = x86_THREAD_STATE64_COUNT;
+    x86_thread_state64_t local_thr_state;
+    STAssertEquals(thread_get_state(thr, x86_THREAD_STATE64, (thread_state_t) &local_thr_state, &state_count), KERN_SUCCESS, @"Failed to fetch thread state");
+    STAssertTrue(memcmp(&thr_state.x86_state.thread.uts.ts64, &local_thr_state, sizeof(thr_state.x86_state.thread.uts.ts64)) == 0, @"Incorrectly copied");
+    STAssertEquals(thr_state.x86_state.thread.tsh.count, (int)x86_THREAD_STATE64_COUNT, @"Incorrect thread state count for a 64-bit system");
+    STAssertEquals(thr_state.x86_state.thread.tsh.flavor, (int)x86_THREAD_STATE64, @"Incorrect thread state flavor for a 64-bit system");
+
+    state_count = x86_EXCEPTION_STATE64_COUNT;
+    x86_exception_state64_t local_exc_state;
+    STAssertEquals(thread_get_state(thr, x86_EXCEPTION_STATE64, (thread_state_t) &local_exc_state, &state_count), KERN_SUCCESS, @"Failed to fetch thread state");
+    STAssertTrue(memcmp(&thr_state.x86_state.exception.ues.es64, &local_exc_state, sizeof(thr_state.x86_state.exception.ues.es64)) == 0, @"Incorrectly copied");
+    STAssertEquals(thr_state.x86_state.exception.esh.count, (int) x86_EXCEPTION_STATE64_COUNT, @"Incorrect thread state count for a 64-bit system");
+    STAssertEquals(thr_state.x86_state.exception.esh.flavor, (int) x86_EXCEPTION_STATE64, @"Incorrect thread state flavor for a 64-bit system");
+
+#elif defined(PLFRAME_X86_SUPPORT)
+    state_count = x86_THREAD_STATE32_COUNT;
+    x86_thread_state32_t local_thr_state;
+    STAssertEquals(thread_get_state(thr, x86_THREAD_STATE32, (thread_state_t) &local_thr_state, &state_count), KERN_SUCCESS, @"Failed to fetch thread state");
+    STAssertTrue(memcmp(&thr_state.x86_state.thread.uts.ts32, &local_thr_state, sizeof(thr_state.x86_state.thread.uts.ts64)) == 0, @"Incorrectly copied");
+    STAssertEquals(thr_state.x86_state.thread.tsh.count, (int)x86_THREAD_STATE32_COUNT, @"Incorrect thread state count for a 64-bit system");
+    STAssertEquals(thr_state.x86_state.thread.tsh.flavor, (int)x86_THREAD_STATE32, @"Incorrect thread state flavor for a 32-bit system");
+
+    state_count = x86_EXCEPTION_STATE32_COUNT;
+    x86_exception_state32_t local_exc_state;
+    STAssertEquals(thread_get_state(thr, x86_EXCEPTION_STATE32, (thread_state_t) &local_exc_state, &state_count), KERN_SUCCESS, @"Failed to fetch thread state");
+    STAssertTrue(memcmp(&thr_state.x86_state.exception.ues.es32, &local_exc_state, sizeof(thr_state.x86_state.exception.ues.es32)) == 0, @"Incorrectly copied");
+    STAssertEquals(thr_state.x86_state.exception.esh.count, (int) x86_EXCEPTION_STATE32_COUNT, @"Incorrect thread state count for a 32-bit system");
+    STAssertEquals(thr_state.x86_state.exception.esh.flavor, (int) x86_EXCEPTION_STATE32, @"Incorrect thread state flavor for a 32-bit system");
+#else
+#error Add platform support
+#endif
+
+    /* Clean up */
+    thread_resume(thr);
+    plframe_test_thread_stop(&test_thr);
+}
+
 /* test plframe_cursor_init() */
 - (void) testInitFrame {
     plframe_cursor_t cursor;
