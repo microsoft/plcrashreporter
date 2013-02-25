a8f029454747739b1730da774ef19e5d6a07ab6a
Landon Fuller <landonf@plausible.coop>
2013-02-25 16:43:23 -0500

    Add a test for stack direction.

--- Source/PLCrashFrameStackUnwindTests.m
+++ Source/PLCrashFrameStackUnwindTests.m
@@ -35,6 +35,12 @@ struct stack_frame {
     uintptr_t pc;
 } __attribute__((packed));
 
+/**
+ * @internal
+ *
+ * This code tests stack-based frame unwinding. It currently assumes that the stack grows down (which is
+ * true on the architectures we currently support, but TODO: this should be direction-neutral).
+ */
 @interface PLCrashFrameStackUnwindTests : SenTestCase @end
 
 @implementation PLCrashFrameStackUnwindTests
@@ -73,6 +79,42 @@ struct stack_frame {
     
     /* Ensure that the final frame's NULL fp triggers an ENOFRAME */
     STAssertEquals(plframe_cursor_next(&cursor), PLFRAME_ENOFRAME, @"Expected to hit end of frames");
+}
+
+/**
+ * Verify that walking terminates with frame address greater than the current address.
+ */
+- (void) testStackDirection {
+    /* Set up test stack */
+    struct stack_frame frames[] = {
+        { .fp = &frames[1], .pc = 0x1 },
+        { .fp = &frames[0], .pc = 0x2 },
+        { .fp = &frames[1], .pc = 0x3 },
+    };
+    size_t frame_count = sizeof(frames) / sizeof(frames[0]);
+    
+    /* Configure thread state */
+    plcrash_async_thread_state_t state;
+    memset(&state, 0, sizeof(state));
+    plcrash_async_thread_state_set_reg(&state, PLCRASH_REG_FP, frames[frame_count-1].fp);
+    plcrash_async_thread_state_set_reg(&state, PLCRASH_REG_IP, frames[frame_count-1].pc);
+    
+    /* Try walking the stack */
+    plframe_cursor_t cursor;
+    plcrash_greg_t reg;
+    
+    plframe_cursor_init(&cursor, mach_task_self(), &state);
+    
+    for (size_t i = 0; i < frame_count; i++) {
+        size_t idx = frame_count-i-1;
+        
+        STAssertEquals(plframe_cursor_next_fp(&cursor), PLFRAME_ESUCCESS, @"Failed to step cursor");
+        STAssertEquals(plframe_cursor_get_reg(&cursor, PLCRASH_REG_IP, &reg), PLFRAME_ESUCCESS, @"Failed to fetch IP");
+        STAssertEquals(reg, (plcrash_greg_t)frames[idx].pc, @"Incorrect IP");
+    }
+
+    /* Ensure that the final frame's bad fp triggers an EBADFRAME */
+    STAssertEquals(plframe_cursor_next(&cursor), PLFRAME_EBADFRAME, @"Expected to hit end of frames");
 }
 
 @end
