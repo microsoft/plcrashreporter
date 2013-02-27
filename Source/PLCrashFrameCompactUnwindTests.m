3dabed87de6487af807b2b6dbcdfcc6a55124677
Landon Fuller <landonf@plausible.coop>
2013-02-27 14:34:15 -0500

    Stub out image loading.
    These tests currently fail due to the lack of FAT binary support in the
    Mach-O APIs (these APIs assume that they are working on actual loaded
    images, which will never be fat).

--- Source/PLCrashFrameCompactUnwindTests.m
+++ Source/PLCrashFrameCompactUnwindTests.m
@@ -27,6 +27,19 @@
 
 #import "GTMSenTestCase.h"
 #import "PLCrashFrameCompactUnwind.h"
+#import "PLCrashAsyncMachOImage.h"
+
+#import <TargetConditionals.h>
+
+#if TARGET_OS_MAC && (!TARGET_OS_IPHONE)
+#define TEST_BINARY @"test.macosx"
+#elif TARGET_OS_SIMULATOR
+#define TEST_BINARY @"test.sim"
+#elif TARGET_OS_IPHONE
+#define TEST_BINARY @"test.ios"
+#else
+#error Unsupported target
+#endif
 
 /**
  * @internal
@@ -36,4 +49,57 @@
 @interface PLCrashFrameCompactUnwindTests : SenTestCase @end
 
 @implementation PLCrashFrameCompactUnwindTests
+
+/**
+ * Return the full path to the request test resource.
+ *
+ * @param resourceName Relative resource path.
+ *
+ * Test resources are located in Bundle/Resources/Tests/TestClass/ResourceName
+ */
+- (NSString *) pathForTestResource: (NSString *) resourceName {
+    NSString *className = NSStringFromClass([self class]);
+    NSString *bundleResources = [[NSBundle bundleForClass: [self class]] resourcePath];
+    NSString *testResources = [bundleResources stringByAppendingPathComponent: @"Tests"];
+    NSString *testRoot = [testResources stringByAppendingPathComponent: className];
+
+    return [testRoot stringByAppendingPathComponent: resourceName];
+}
+
+/**
+ * Find the test resource with the given @a resourceName, and load the resource's data.
+ *
+ * @param resourceName Relative resource path.
+ */
+- (NSData *) dataForTestResource: (NSString *) resourceName {
+    NSError *error;
+    NSString *path = [self pathForTestResource: resourceName];
+    NSData *result = [NSData dataWithContentsOfFile: path options: NSDataReadingMappedIfSafe error: &error];
+    NSAssert(result != nil, @"Failed to load resource data: %@", error);
+    
+    return result;
+}
+
+- (void) testSomething {
+#if 0
+    /* Load the image into a memory object */
+    NSData *mappedImage = [self dataForTestResource: TEST_BINARY];
+    plcrash_async_mobject_t imageData;
+    plcrash_async_mobject_init(&imageData, mach_task_self(), (pl_vm_address_t) [mappedImage bytes], [mappedImage length]);
+
+    /* Parse the image */
+    plcrash_async_macho_t image;
+    plcrash_async_mobject_t sect;
+    plcrash_error_t err;
+
+    err = plcrash_nasync_macho_init(&image, mach_task_self(), [TEST_BINARY UTF8String], imageData.address, 0);
+    STAssertEquals(err, PLCRASH_ESUCCESS, @"Failed to initialize Mach-O parser");
+
+    err = plcrash_async_macho_map_section(&image, SEG_TEXT, "__unwind_info", &sect);
+    STAssertEquals(err, PLCRASH_ESUCCESS, @"Failed to map unwind info");
+
+    plcrash_async_mobject_free(&sect);
+#endif
+}
+
 @end
