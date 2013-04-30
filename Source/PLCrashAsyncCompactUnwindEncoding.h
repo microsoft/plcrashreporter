2820b8b852ed898f13647e44af0b81cb8c3ef1e3
Landon Fuller <landonf@plausible.coop>
2013-04-30 11:03:52 -0400

    Define the CFE entry types.

--- Source/PLCrashAsyncCompactUnwindEncoding.h
+++ Source/PLCrashAsyncCompactUnwindEncoding.h
@@ -55,6 +55,59 @@ typedef struct plcrash_async_cfe_reader {
     const plcrash_async_byteorder_t *byteorder;
 } plcrash_async_cfe_reader_t;
 
+/**
+ * Supported CFE entry formats.
+ */
+typedef enum {
+    /**
+     * The frame pointer (fp) is valid. To walk the stack, the previous frame pointer may be popped from
+     * the current frame pointer, followed by the return address.
+     *
+     * All non-volatile registers that need to be restored will be saved on the stack, ranging from fp±regsize through
+     * fp±1020. The actual direction depends on the stack growth direction of the target platform.
+     */
+    PLCRASH_ASYNC_CFE_ENTRY_TYPE_FRAME_PTR = 1,
+
+    /**
+     * The frame pointer (eg, ebp/rbp) is invalid, but the stack size is constant and is small enough (<= 1024) that it
+     * may be encoded in the CFE entry itself.
+     *
+     * The return address may be found at the provided ± offset from the stack pointer, followed all non-volatile
+     * registers that need to be restored. The actual direction of the offset epends on the stack growth direction of
+     * the target platform.
+     */
+    PLCRASH_ASYNC_CFE_ENTRY_TYPE_FRAMELESS_IMMD = 2,
+
+    /**
+     * The frame pointer (eg, ebp/rbp) is invalid, but the stack size is constant and is too large (>= 1024) to be
+     * encoded in the CFE entry itself. Instead, the fixed stack size value must be extracted from an actual instruction
+     * (eg, subl) within the target function, and used as the constant stack size. The decoded stack offset may be
+     * added to the start address of the function to determine the location of the actual stack size.
+     *
+     * The return address may be found at the derived ± offset from the stack pointer, followed all non-volatile
+     * registers that need to be restored. The actual direction of the offset epends on the stack growth direction of
+     * the target platform.
+     */
+    PLCRASH_ASYNC_CFE_ENTRY_TYPE_FRAMELESS_INDIRECT = 3,
+
+    /**
+     * The unwinding information for the target address could not be encoded using the CFE format. Instead, DWARF
+     * frame information must be used.
+     *
+     * An offset to the DWARF FDE in the __eh_frame section is be provided.
+     */
+    PLCRASH_ASYNC_CFE_ENTRY_TYPE_DWARF = 4
+} plcrash_async_cfe_entry_type_t;
+
+/**
+ * A decoded CFE entry. The entry represents the data necessary to unwind the stack frame at a given PC, including
+ * restoration of saved registers.
+ */
+typedef struct plcrash_async_cfe_entry {
+    /** The CFE entry type. */
+    plcrash_async_cfe_entry_type_t type;
+} plcrash_async_cfe_entry_t;
+
 plcrash_error_t plcrash_async_cfe_reader_init (plcrash_async_cfe_reader_t *reader, plcrash_async_mobject_t *mobj, cpu_type_t cputype);
 
 plcrash_error_t plcrash_async_cfe_reader_find_pc (plcrash_async_cfe_reader_t *reader, pl_vm_address_t pc, uint32_t *encoding);
