6fc958464d4e43c3dd19c95d3b6065c67e6b2de7
Landon Fuller <landonf@plausible.coop>
2013-05-22 18:05:01 -0400

    Stub out loading of eh_frame/debug_frame sections.

--- Source/PLCrashAsyncDwarfEncodingTests.m
+++ Source/PLCrashAsyncDwarfEncodingTests.m
@@ -26,14 +26,72 @@
  * OTHER DEALINGS IN THE SOFTWARE.
  */
 
-#import "GTMSenTestCase.h"
+#import "PLCrashTestCase.h"
 
 #include "PLCrashAsyncDwarfEncoding.h"
 
 
-@interface PLCrashAsyncDwarfEncodingTests : SenTestCase {
+@interface PLCrashAsyncDwarfEncodingTests : PLCrashTestCase {
 }
 @end
 
 @implementation PLCrashAsyncDwarfEncodingTests
+
+- (void) setUp {
+
+}
+
+- (void) tearDown {
+    
+}
+
+- (void) testSomething {
+    NSError *error;
+    plcrash_error_t err;
+    
+    NSString *binPath = [self pathForTestResource: @"bins"];
+    NSArray *cases = [[NSFileManager defaultManager] contentsOfDirectoryAtPath: binPath error: &error];
+    STAssertNotNil(cases, @"Failed to read test case directory: %@", error);
+    
+    for (NSString *tcase in cases) {
+        plcrash_async_macho_t image;
+        
+        /* Load and parse the image. */
+        NSString *tcasePath = [binPath stringByAppendingPathComponent: tcase];
+        NSData *mappedImage = [NSData dataWithContentsOfFile: tcasePath options: NSDataReadingMapped error: &error];
+        STAssertNotNil(mappedImage, @"Failed to map image: %@", error);
+
+        err = plcrash_nasync_macho_init(&image, mach_task_self(), [tcasePath UTF8String], [mappedImage bytes]);
+        STAssertEquals(err, PLCRASH_ESUCCESS, @"Failed to initialize Mach-O parser");
+        
+        
+        /* Map the (optional) eh/debug DWARF sections. */
+        plcrash_async_mobject_t eh_frame;
+        plcrash_async_mobject_t debug_frame;
+        BOOL has_eh_frame = NO;
+        BOOL has_debug_frame = NO;
+
+        err = plcrash_async_macho_map_section(&image, "__DWARF", "__eh_frame", &eh_frame);
+        if (err != PLCRASH_ENOTFOUND) {
+            STAssertEquals(err, PLCRASH_ESUCCESS, @"Failed to map __eh_frame section for %@", tcase);
+            has_eh_frame = YES;
+        }
+
+        err = plcrash_async_macho_map_section(&image, "__DWARF", "__debug_frame", &debug_frame);
+        if (err != PLCRASH_ENOTFOUND) {
+            STAssertEquals(err, PLCRASH_ESUCCESS, @"Failed to map __debug_frame section for %@", tcase);
+            has_debug_frame = YES;
+        }
+
+        /* Clean up */
+        if (has_eh_frame)
+            plcrash_async_mobject_free(&eh_frame);
+        
+        if (has_debug_frame)
+            plcrash_async_mobject_free(&debug_frame);
+
+        plcrash_nasync_macho_free(&image);
+    }
+}
+
 @end
