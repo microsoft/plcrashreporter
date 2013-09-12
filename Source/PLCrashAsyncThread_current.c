f1d25962c87c0c55264aa24b2641ff91a2c261e7
Landon Fuller <landonf@plausible.coop>
2013-09-11 22:15:51 -0400

    Add an ARM64 plcrash_async_thread_state_current() assembly trampoline.
    Since we don't have ARM64 hardware, this remains untested, and remains
    to be validated against real hardware.
    
    Issue: PLCR-495

--- Source/PLCrashAsyncThread_current.c
+++ Source/PLCrashAsyncThread_current.c
@@ -75,48 +75,42 @@ plcrash_error_t plcrash_async_thread_state_current_stub (plcrash_async_thread_st
 /* sizeof(struct mcontext) */
 VALIDATE(MCONTEXT_SIZE, sizeof(pl_mcontext_t) == PL_MCONTEXT_SIZE);
 
+/* VOFF() is used to assert the structure offsets as required by our assembly trampoline */
+#define OFF(struct, reg, offset) (offsetof(pl_mcontext_t, __##struct.__##reg) == offset)
+#define VOFF(struct, reg, offset) VALIDATE(MCONTEXT_SS_OFFSET_##reg##_, OFF(struct, reg, offset))
+
 #if __x86_64__
 
 /* There's a hard-coded dependency on this size in the trampoline assembly, so we explicitly validate it here. */
 VALIDATE(MCONTEXT_SIZE, sizeof(_STRUCT_MCONTEXT) == 712);
 
 /* Verify the expected offsets */
-#define OFF(reg, offset) (offsetof(_STRUCT_MCONTEXT, __ss.__##reg) == offset)
-#define VOFF(reg, offset) VALIDATE(MCONTEXT_SS_OFFSET_##reg##_, OFF(reg, offset))
-
-VOFF(rax, 16);
-VOFF(rbx, 24);
-VOFF(rcx, 32);
-VOFF(rdx, 40);
-VOFF(rdi, 48);
-VOFF(rsi, 56);
-VOFF(rbp, 64);
-VOFF(rsp, 72);
-VOFF(r8, 80);
-VOFF(r9, 88);
-VOFF(r10, 96);
-VOFF(r11, 104);
-VOFF(r12, 112);
-VOFF(r13, 120);
-VOFF(r14, 128);
-VOFF(r15, 136);
-VOFF(rip, 144);
-VOFF(rflags, 152);
-VOFF(cs, 160);
-VOFF(fs, 168);
-VOFF(gs, 176);
-
-#undef OFF
-#undef VOFF
+VOFF(ss, rax, 16);
+VOFF(ss, rbx, 24);
+VOFF(ss, rcx, 32);
+VOFF(ss, rdx, 40);
+VOFF(ss, rdi, 48);
+VOFF(ss, rsi, 56);
+VOFF(ss, rbp, 64);
+VOFF(ss, rsp, 72);
+VOFF(ss, r8, 80);
+VOFF(ss, r9, 88);
+VOFF(ss, r10, 96);
+VOFF(ss, r11, 104);
+VOFF(ss, r12, 112);
+VOFF(ss, r13, 120);
+VOFF(ss, r14, 128);
+VOFF(ss, r15, 136);
+VOFF(ss, rip, 144);
+VOFF(ss, rflags, 152);
+VOFF(ss, cs, 160);
+VOFF(ss, fs, 168);
+VOFF(ss, gs, 176);
 
 #elif __i386__
 
 /* There's a hard-coded dependency on this size in the trampoline assembly, so we explicitly validate it here. */
 VALIDATE(MCONTEXT_SIZE, sizeof(_STRUCT_MCONTEXT) == 600);
-
-/* Verify the expected offsets */
-#define OFF(struct, reg, offset) (offsetof(_STRUCT_MCONTEXT, __##struct.__##reg) == offset)
-#define VOFF(struct, reg, offset) VALIDATE(MCONTEXT_SS_OFFSET_##reg##_, OFF(struct, reg, offset))
 
 VOFF(ss, eax, 12);
 VOFF(ss, ebx, 16);
@@ -137,8 +131,18 @@ VOFF(ss, gs, 72);
 
 VOFF(es, trapno, 0);
 
-#undef OFF
-#undef VOFF
+#elif defined(__arm64__)
+
+/* There's a hard-coded dependency on this size in the trampoline assembly, so we explicitly validate it here. */
+PLCF_ASSERT_STATIC(MCONTEXT_SIZE, sizeof(pl_mcontext_t) == 816);
+
+/* Verify the expected offsets */
+VOFF(ss, x, 16);
+VOFF(ss, fp, 248);
+VOFF(ss, lr, 256);
+VOFF(ss, sp, 264);
+VOFF(ss, pc, 272);
+VOFF(ss, cpsr, 280);
 
 #elif defined(__arm__)
 
@@ -146,18 +150,11 @@ VOFF(es, trapno, 0);
 VALIDATE(MCONTEXT_SIZE, sizeof(_STRUCT_MCONTEXT) == 340);
 
 /* Verify the expected offsets */
-#define OFF(struct, reg, offset) (offsetof(_STRUCT_MCONTEXT, __##struct.__##reg) == offset)
-#define VOFF(struct, reg, offset) VALIDATE(MCONTEXT_SS_OFFSET_##reg##_, OFF(struct, reg, offset))
-
-
 VOFF(ss, r, 12);
 VOFF(ss, sp, 64);
 VOFF(ss, lr, 68);
 VOFF(ss, pc, 72);
 VOFF(ss, cpsr, 76);
-
-#undef OFF
-#undef VOFF
 
 #else
 
