0b4af4cd1720c8dafc55cc3db0088454dcc723bb
Landon Fuller <landonf@plausible.coop>
2014-03-28 12:56:42 -0400

    Implement Mac demo
    Issue: PLCR-534

--- OnErrorResumeExample-Mac/OnErrorResumeExample/PLAppDelegate.m
+++ OnErrorResumeExample-Mac/OnErrorResumeExample/PLAppDelegate.m
@@ -8,11 +8,54 @@
 
 #import "PLAppDelegate.h"
 
-@implementation PLAppDelegate
+#import <CrashReporter/CrashReporter.h>
 
-- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
-{
-    // Insert code here to initialize your application
+@implementation PLAppDelegate {
+    PLCrashReporter *_reporter;
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    PLCrashReporterConfig *config = [[PLCrashReporterConfig alloc] initWithSignalHandlerType: PLCrashReporterSignalHandlerTypeBSD
+                                                                       symbolicationStrategy: PLCrashReporterSymbolicationStrategyNone
+                                                                               onErrorResume: YES];
+    
+    _reporter = [[PLCrashReporter alloc] initWithConfiguration: config];
+    [_reporter enableCrashReporter];
+}
+
+- (IBAction) cfreleaseMe: (id) sender {
+    /* CFRelease assertion! */
+    CFRelease(NULL);
+}
+
+- (IBAction) sunyata:(id)sender {
+    /* I am become NULL */
+    ((volatile char *) NULL)[0] = 0xFF;
+}
+
+- (IBAction) firstContact:(id)sender {
+    /* An object cache? Maybe. Or maybe we're from another universe */
+    void *cache[] = {
+        NULL, NULL, NULL
+    };
+    
+    void *displayStrings[6] = {
+        "This little piggy went to the market",
+        "This little piggy stayed at home",
+        cache,
+        "This little piggy had roast beef.",
+        "This little piggy had none.",
+        "And this little piggy went 'Wee! Wee! Wee!' all the way home",
+    };
+    
+    /* A corrupted/under-retained/re-used piece of memory */
+    struct {
+        void *isa;
+    } corruptObj;
+    corruptObj.isa = displayStrings;
+
+    /* Send a message into the unknown */
+    [((__bridge id) (void *) 0x5) description];
 }
 
 @end
