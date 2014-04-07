77e7025108613898991a04ee02d8188e5f771d88
Landon Fuller <landonf@plausible.coop>
2014-04-07 11:49:26 -0400

    Stub out an iterator API compatible with C++11's ranged for loop.
    Issue: PLCR-527

---
+++ Source/Iterator.hpp
@@ -0,0 +1,87 @@
+/*
+ * Copyright (c) 2014 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef PLCRASH_ASYNC_ITERATOR_H
+#define PLCRASH_ASYNC_ITERATOR_H
+
+/**
+ * @internal
+ * @ingroup plcrash_async
+ * @{
+ */
+
+namespace plcrash { namespace async {
+
+/**
+ * An async-safe data structure that supports forward iteration over a sequence of
+ * elements, with support for C++11 range-based for loop syntax.
+ *
+ * Like C++ STL, termination of iteration is performed via comparison against a 'terminal'
+ * iterator instance.
+ *
+ * @tparam T The iterator's value type.
+ * @tparam Pointer Pointer type of T.
+ * @tparam Reference Reference type of T.
+ *
+ * @note The design of this API -- specifically, use of increment, dereference, and equality operators --
+ * may be considered suboptimal, but is necessary to maintain compatibility with C++11's
+ * STL-style range-based for loop support.
+ */
+template <class T, class Pointer = const T*, class Reference = const T&> class Iterator {
+public:    
+    /** Advance the iterator by one position and return the original iterator value. */
+    Iterator &operator++ ();
+    
+    /** Advance the iterator by one position and return the new iterator value. */
+    Iterator operator++ (int);
+    
+    /** Return a reference to the element at the iterator's current position. */
+    Reference operator* () const;
+    
+    /** Return a pointer to the element at the iterator's current position. */
+    Pointer operator-> () const;
+    
+    /** 
+     * Return true if this instance is equal to @a other.
+     *
+     * @param other The iterator to compare against. This may be the terminal iterator.
+     */
+    bool operator== (const Iterator &other) const;
+    
+    /**
+     * Return true if this instance is equal to @a other.
+     *
+     * @param other The iterator to compare against. This may be the terminal iterator.
+     */
+    bool operator!= (const Iterator &other) const;
+};
+    
+}} /* namespace plcrash::async */
+
+
+/** @} */
+
+#endif /* PLCRASH_ASYNC_ITERATOR_H */
---
+++ Source/IteratorTests.mm
@@ -0,0 +1,139 @@
+/*
+ * Author: Landon Fuller <landonf@plausible.coop>
+ *
+ * Copyright (c) 2008-2013 Plausible Labs Cooperative, Inc.
+ * All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#import "GTMSenTestCase.h"
+
+#include "Iterator.hpp"
+
+using namespace plcrash::async;
+
+@interface IteratorTests : SenTestCase @end
+
+@implementation IteratorTests
+
+/**
+ * A class that represents a range of integers.
+ */
+class IntRange {
+private:
+    int _start;
+    int _end;
+    
+public:
+    /* A simple iterator; walks a consequence range of integers */
+    class iterator : public Iterator<int> {
+    private:
+        int _value;
+        
+    public:
+        iterator (int value) {
+            _value = value;
+        };
+        
+        // from Iterator
+        iterator operator++ () {
+            iterator tmp = *this;
+            _value++;
+            return tmp;
+        }
+        
+        // from Iterator
+        iterator operator++ (int) {
+            _value++;
+            return *this;
+        }
+        
+        // from Iterator
+        int operator* () const {
+            return _value;
+        }
+        
+        // from Iterator
+        int operator-> () const {
+            return _value;
+        }
+        
+        // from Iterator
+        bool operator== (const iterator &other) const {
+            return other._value == _value;
+        };
+        
+        // from Iterator
+        bool operator!= (const iterator &other) const {
+            return !(*this == other);
+        };
+    };
+    
+    /**
+     * Construct a new IntRange instance.
+     *
+     * @param start The (inclusive) start of the range.
+     * @param end The (exclusive) end of the range.
+     */
+    IntRange (int start, int end) {
+        _start = start;
+        _end = end;
+    };
+    
+    /** Return an iterator pointing to the first element in the range */
+    iterator begin () {
+        return iterator(_start);
+    };
+
+    /** Returns a iterator pointing to the past-the-end element in the range. */
+    iterator end () {
+        return iterator(_end);
+    };
+    
+};
+
+
+- (void) testIteration {
+    const int count = 10;
+
+    IntRange range = IntRange(0, count);
+    
+    int values[count];
+    memset(values, 0, sizeof(values));
+
+    /* Try iterating the range */
+    int vpos = 0;
+    for (int i : range) {
+        STAssertTrue(vpos < count, @"Overran the expected range");
+
+        values[vpos] = i;
+        vpos++;
+    }
+    
+    /* Verify the results */
+    for (int i = 0; i < count; i++) {
+        STAssertEquals(values[i], i, @"Incorrect value was iterated");
+    }
+}
+
+@end
