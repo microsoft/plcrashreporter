99525dc9cc62fe981c798428d37ef522faede550
Landon Fuller <landonf@plausible.coop>
2014-04-10 16:15:00 -0400

    Additional tests and and memory over-read validation.
    This adds a few improvements recommended by Mike Ash:
    
    - Tests for 0-length strings, as well as strings that start with
      invalid byte sequences.
    - A minor rework of the tests to lay out the test string bytes at the
      end of a page, directly before a PROT_NONE page. This allows us to use
      page protections to assert that over-read does not occur.
    
    Issue: PLCR-532

--- Source/PLCrashSysctlTests.m
+++ Source/PLCrashSysctlTests.m
@@ -53,35 +53,101 @@
     STAssertEquals(result, (int)[[NSProcessInfo processInfo] processorCount], @"Incorrect count");
 }
 
+/**
+ * @internal
+ *
+ * Internal implementation of the MAX_LEN_UTF8() and LEN_UTF8() macros used in PLCrashSysctlTests::testValidUTF8Strlen.
+ *
+ * Handles writing of the test data at the end of the first page within @a test_pages, where
+ * an overrun will trigger an access violation, and then returning the result of calling
+ * plcrash_sysctl_valid_utf8_bytes() or plcrash_sysctl_valid_utf8_bytes_max() on the written data.
+ *
+ * We could also implement this using GCC's non-standard statement exprs, rather than using an additional function.
+ *
+ * @param test_pages Page-aligned allocation of size PAGE_SIZE*2. The second page should be set PROT_NONE to ensure
+ * that over-read triggers an access violation.
+ * @param test_data The test data to write at the end of the first page within @a test_pages.
+ * @param test_data_len The length of @a test_data in bytes.
+ * @param maxvariant If true, plcrash_sysctl_valid_utf8_bytes_max() will be called. If false, plcrash_sysctl_valid_utf8_bytes().
+ * @param maxlen The maxlen value to be passed to plcrash_sysctl_valid_utf8_bytes_max().
+ */
+static size_t utf8_test_data_strlen_max (uint8_t *test_pages, uint8_t *test_data, size_t test_data_len, BOOL maxvariant, size_t maxlen) {
+    assert(test_data_len < PAGE_SIZE);
+
+    uint8_t *target = test_pages + (PAGE_SIZE - test_data_len);
+    memcpy(target, test_data, test_data_len);
+    
+    if (maxvariant) {
+        return plcrash_sysctl_valid_utf8_bytes_max(target, maxlen);
+    } else {
+        return plcrash_sysctl_valid_utf8_bytes(target);
+    }
+}
+
 /* Test handling of truncated UTF-8 strings */
 - (void) testValidUTF8Strlen {
+    /*
+     * For our tests, we set up a custom two-page allocation to detect read overruns.
+     *
+     * The first page has R|W permissions, while the second page is set to PROT_NONE; by
+     * positioning our test data on the trailing edge of the first page, we let the second
+     * page trigger an access violation.
+     *
+     * The allocation is cleaned up at the bottom of this method.
+     */
+    uint8_t *test_pages;
+    {
+        /* Allocate the two pages */
+        test_pages = mmap(NULL, PAGE_SIZE*2, PROT_READ|PROT_WRITE, MAP_ANON|MAP_PRIVATE, -1, 0);
+        STAssertNotEquals((void *)test_pages, MAP_FAILED, @"Failed to mmap() pages: %s", strerror(errno));
+        
+        /* Mark the second page as non-writable, non-readable */
+        STAssertEquals(0, mprotect(test_pages+PAGE_SIZE, PAGE_SIZE, PROT_NONE), @"Failed to set page protections: %s", strerror(errno));
+    }
+
+    /* Position the given bytes within our test_pages buffer, returning the result of plcrash_sysctl_valid_utf8_bytes_max(). */
+#define MAX_LEN_UTF8(maxlen, ...) \
+    utf8_test_data_strlen_max(test_pages, (uint8_t[]){ __VA_ARGS__ }, sizeof((uint8_t[]){ __VA_ARGS__ }), true, maxlen)
+
     /* Test handling (and interaction) of maxlen and NUL termination */
     {
-        STAssertEquals(plcrash_sysctl_valid_utf8_bytes_max((uint8_t[]) {'a', '\0'} , 100), (size_t)1,
-                       @"String iteration ignored NUL terminator in favor of maxlen");
+        STAssertEquals(MAX_LEN_UTF8(100, 'a', '\0'), (size_t)1, @"String iteration ignored NUL terminator in favor of maxlen");
 
-        STAssertEquals(plcrash_sysctl_valid_utf8_bytes_max((uint8_t[]) {'a', 'a', '\0'}, 1), (size_t)1,
-                       @"String iteration ignored maxlen in favor of NUL");
+        STAssertEquals(MAX_LEN_UTF8(1, 'a', 'a', '\0'), (size_t)1, @"String iteration ignored maxlen in favor of NUL");
     }
     
     /* Test handling (and interaction) of maxlen and multibyte validation */
     {
         /* This is a valid multibyte sequences; we use maxlen to terminate in the middle of it */
-        STAssertEquals(plcrash_sysctl_valid_utf8_bytes_max((uint8_t[]) {'a', 0xC2, 0x80, '\0'}, 2), (size_t)1,
-                       @"Multibyte validation ignored maxlen");
+        STAssertEquals(MAX_LEN_UTF8(2, 'a', 0xC2, 0x80, '\0'), (size_t)1, @"Multibyte validation ignored maxlen");
         
         /* Verify that maxlen doesn't trigger *early* termination. This also sanity-checks the above test,
          * asserting that had maxlen not been set too low, the characters would have been correctly validated */
-        STAssertEquals(plcrash_sysctl_valid_utf8_bytes_max((uint8_t[]) {'a', 0xC2, 0x80, '\0'}, 3), (size_t)3,
-                       @"Maxlen value triggered incorrect early termination of multibyte validation");
+        STAssertEquals(MAX_LEN_UTF8(3, 'a', 0xC2, 0x80, '\0'), (size_t)3, @"Maxlen value triggered incorrect early termination of multibyte validation");
     }
 
 #define LEN_UTF8(...) \
-    plcrash_sysctl_valid_utf8_bytes((uint8_t[]) {__VA_ARGS__})
+    utf8_test_data_strlen_max(test_pages, (uint8_t[]){ __VA_ARGS__ }, sizeof((uint8_t[]){ __VA_ARGS__ }), false, 0)
 
     /* Test BOM handling. BOM is not useful or recommended for UTF-8 encoding, but it's still necessary to support. */
     {
         STAssertEquals(LEN_UTF8(0xEF, 0xBB, 0xBF, 'a', '\0'), (size_t)4, @"0 continutation rejected in-range byte");
+    }
+    
+    /* Test handling of a 0-length string. */
+    STAssertEquals(LEN_UTF8('\0'), (size_t)0, @"0 length NULL terminated string should return 0");
+    
+    /* Test handling of a 0-length maxlen on a non-zero length string. */
+    STAssertEquals(MAX_LEN_UTF8(0, 'a', '\0'), (size_t)0,
+                   @"String iteration ignored maxlen of a zero length string");
+    
+    /* Test handling of strings that start with invalid UTF-8 byte(s) */
+    {
+        /* Invalid single byte character */
+        STAssertEquals(LEN_UTF8(128, '\0'), (size_t)0, @"Length should be zero on a string starting with invalid UTF-8");
+        
+        /* Missing continuation */
+        STAssertEquals(LEN_UTF8(0xC0, '\0'), (size_t)0, @"Length should be zero on a string starting with invalid UTF-8");
     }
 
     /* Test 0 continuation sequence */
@@ -147,6 +213,10 @@
     }
 
 #undef LEN_UTF8
+#undef MAX_LEN_UTF8
+    
+    /* Clean up */
+    munmap(test_pages, PAGE_SIZE*2);
 }
 
 @end
