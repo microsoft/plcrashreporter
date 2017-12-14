5dd94a0538694fab2da49b59bfe9d828248935c2
yongseok kang <yongseok.kang@navercorp.com>
2017-12-14 11:49:05 +0900

    PLCrashReport support to dynamic framework

---
+++ CrashReporter-Framework/CrashReporter_Framework.h
@@ -0,0 +1,18 @@
+//
+//  CrashReporter_Framework.h
+//  CrashReporter-Framework
+//
+//  Created by KangYongseok on 14/12/2017.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrashReporter_Framework.
+FOUNDATION_EXPORT double CrashReporter_FrameworkVersionNumber;
+
+//! Project version string for CrashReporter_Framework.
+FOUNDATION_EXPORT const unsigned char CrashReporter_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrashReporter_Framework/PublicHeader.h>
+
+
