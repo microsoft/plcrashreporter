8a081573ef1ebab6a3eaba7da331c880585099e4
yongseok kang <yongseok.kang@navercorp.com>
2018-08-16 09:36:29 +0900

    Add tvOS, watchOS targets

---
+++ CrashReporter-tvOS-Framework/CrashReporter_tvOS_Framework.h
@@ -0,0 +1,18 @@
+//
+//  CrashReporter_tvOS_Framework.h
+//  CrashReporter-tvOS-Framework
+//
+//  Created by KangYongseok on 16/08/2018.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrashReporter_tvOS_Framework.
+FOUNDATION_EXPORT double CrashReporter_tvOS_FrameworkVersionNumber;
+
+//! Project version string for CrashReporter_tvOS_Framework.
+FOUNDATION_EXPORT const unsigned char CrashReporter_tvOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrashReporter_tvOS_Framework/PublicHeader.h>
+
+
---
+++ CrashReporter-watchOS-Framework/CrashReporter_watchOS_Framework.h
@@ -0,0 +1,18 @@
+//
+//  CrashReporter_watchOS_Framework.h
+//  CrashReporter-watchOS-Framework
+//
+//  Created by KangYongseok on 16/08/2018.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for CrashReporter_watchOS_Framework.
+FOUNDATION_EXPORT double CrashReporter_watchOS_FrameworkVersionNumber;
+
+//! Project version string for CrashReporter_watchOS_Framework.
+FOUNDATION_EXPORT const unsigned char CrashReporter_watchOS_FrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrashReporter_watchOS_Framework/PublicHeader.h>
+
+
