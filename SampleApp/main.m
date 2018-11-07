78592be8247e0b3ef128978f9850857db836bc2b
yongseok kang <yongseok.kang@navercorp.com>
2018-11-07 13:15:00 +0900

    Add SampleApp

---
+++ SampleApp/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  SampleApp
+//
+//  Created by KangYongseok on 07/11/2018.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
