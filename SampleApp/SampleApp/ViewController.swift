98f21223fe23911dfcde61db44bb591e4413aa54
yongseok kang <yongseok.kang@navercorp.com>
2018-11-07 11:20:52 +0900

    Add SampleApp

---
+++ SampleApp/SampleApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SampleApp
+//
+//  Created by KangYongseok on 07/11/2018.
+//  Copyright © 2018 Yongseok Kang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
