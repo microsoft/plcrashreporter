bca4856055fa307e69dd6c070d5ea1d5a6da5de6
ilya Shumihin <v-ilshum@microsoft.com>
2019-12-11 13:16:22 +0300

    Fix formatting

--- Source/PLCrashLogWriter.m
+++ Source/PLCrashLogWriter.m
@@ -428,10 +428,7 @@ plcrash_error_t plcrash_log_writer_init (plcrash_log_writer_t *writer,
     /* iPhone OS */
     {
         NSOperatingSystemVersion systemVersion = [NSProcessInfo processInfo].operatingSystemVersion;
-
-        NSString *systemVersionString =
-        [NSString stringWithFormat:@"%ld.%ld", (long)systemVersion.majorVersion, (long)systemVersion.minorVersion];
-
+        NSString *systemVersionString = [NSString stringWithFormat:@"%ld.%ld", (long)systemVersion.majorVersion, (long)systemVersion.minorVersion];
         if (systemVersion.patchVersion > 0) {
             systemVersionString = [systemVersionString stringByAppendingFormat:@".%ld", (long)systemVersion.patchVersion];
         }
