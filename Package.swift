accf924e2dcfa120efc01a055d9be83016562882
Ivan Matkov <v-ivmatk@microsoft.com>
2020-05-06 22:06:35 +0300

    Add platforms list to SwiftPM manifest

--- Package.swift
+++ Package.swift
@@ -4,6 +4,11 @@ import PackageDescription
 
 let package = Package(
     name: "PLCrashReporter",
+    platforms: [
+        .iOS(.v8),
+        .macOS(.v10_10),
+        .tvOS(.v9)
+    ],
     products: [
         .library(name: "CrashReporter", targets: ["CrashReporter"])
     ],
