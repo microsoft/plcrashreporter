3389bebb81d941a0f8790c24193cc4f030283e46
Anna Kocheshkova <v-annkoc@microsoft.com>
2020-05-19 10:46:29 +0300

    Fix manual build

--- Scripts/build-xcframework.sh
+++ Scripts/build-xcframework.sh
@@ -3,7 +3,7 @@ set -e
 
 # Build Mac Catalyst.
 # Print only target name and issues. Mimic Xcode output to make prettify tools happy.
-echo "=== BUILD TARGET $1 OF PROJECT ${PROJECT_NAME} WITH CONFIGURATION ${CONFIGURATION} ==="
+echo "=== BUILD TARGET ${PROJECT_NAME} iOS Framework OF PROJECT ${PROJECT_NAME} WITH CONFIGURATION ${CONFIGURATION} ==="
 # OBJROOT must be customized to avoid conflicts with the current process.
 xcodebuild -quiet \
     SYMROOT="${SYMROOT}" OBJROOT="${BUILT_PRODUCTS_DIR}" PROJECT_TEMP_DIR="${PROJECT_TEMP_DIR}" \
@@ -16,4 +16,7 @@ for SDK in iphoneos iphonesimulator appletvos appletvsimulator macOS maccatalyst
   FRAMEWORK_PATH="${BUILD_DIR}/${CONFIGURATION}-${SDK}/${PRODUCT_NAME}.framework"
   XC_FRAMEWORKS="${XC_FRAMEWORKS} -framework ${FRAMEWORK_PATH}"
 done
+
+rm -rf ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.xcframework
+
 xcodebuild -create-xcframework ${XC_FRAMEWORKS} -output ${BUILT_PRODUCTS_DIR}/${PRODUCT_NAME}.xcframework
