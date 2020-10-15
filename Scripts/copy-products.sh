fab304b68652199fff9ad5eba074641ad7178b64
Ivan Matkov <v-ivmatk@microsoft.com>
2020-10-15 16:28:53 +0300

    Fix archive scripts

--- Scripts/copy-products.sh
+++ Scripts/copy-products.sh
@@ -11,18 +11,18 @@ install -m 755 "$BUILD_DIR/$CONFIGURATION-macosx/plcrashutil" "$BUILT_PRODUCTS_D
 
 # Universal frameworks
 mkdir -p "$BUILT_PRODUCTS_DIR/iOS Framework"
-cp -RH "$BUILD_DIR/$CONFIGURATION-iphoneuniversal/$PROJECT_NAME.framework" "$BUILT_PRODUCTS_DIR/iOS Framework"
+cp -R "$BUILD_DIR/$CONFIGURATION-iphoneuniversal/$PROJECT_NAME.framework" "$BUILT_PRODUCTS_DIR/iOS Framework"
 
 mkdir -p "$BUILT_PRODUCTS_DIR/tvOS Framework"
-cp -RH "$BUILD_DIR/$CONFIGURATION-appletvuniversal/$PROJECT_NAME.framework" "$BUILT_PRODUCTS_DIR/tvOS Framework"
+cp -R "$BUILD_DIR/$CONFIGURATION-appletvuniversal/$PROJECT_NAME.framework" "$BUILT_PRODUCTS_DIR/tvOS Framework"
 
 # Dynamic macOS framework
 mkdir -p "$BUILT_PRODUCTS_DIR/Mac OS X Framework"
-cp -RH "$BUILD_DIR/$CONFIGURATION-macosx/$PROJECT_NAME.framework" "$BUILT_PRODUCTS_DIR/Mac OS X Framework"
-cp -RH "$BUILD_DIR/$CONFIGURATION-macosx/$PROJECT_NAME.framework.dSYM" "$BUILT_PRODUCTS_DIR/Mac OS X Framework"
+cp -R "$BUILD_DIR/$CONFIGURATION-macosx/$PROJECT_NAME.framework" "$BUILT_PRODUCTS_DIR/Mac OS X Framework"
+cp -R "$BUILD_DIR/$CONFIGURATION-macosx/$PROJECT_NAME.framework.dSYM" "$BUILT_PRODUCTS_DIR/Mac OS X Framework"
 
 # XCFrameowrk
-cp -RH "$BUILD_DIR/$CONFIGURATION-xcframework/$PROJECT_NAME.xcframework" "$BUILT_PRODUCTS_DIR"
+cp -R "$BUILD_DIR/$CONFIGURATION-xcframework/$PROJECT_NAME.xcframework" "$BUILT_PRODUCTS_DIR"
 
 # Static libraries
 mkdir -p "$BUILT_PRODUCTS_DIR/Static/include"
