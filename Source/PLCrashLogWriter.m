ce4d5b33430eb53bc5ee89ef03d3b3cbabc25e1d
Suman Cherukuri <suman@cherukuris.net>
2021-01-12 14:01:35 -0800

    Fix a crash when you delete custom data and add different custom data

--- Source/PLCrashLogWriter.m
+++ Source/PLCrashLogWriter.m
@@ -275,7 +275,7 @@ static void plprotobuf_cbinary_data_nsstring_init (PLProtobufCBinaryData *data, 
 static void plprotobuf_cbinary_data_free (PLProtobufCBinaryData *data) {
     if (data != NULL && data->data != NULL) {
         free(data->data);
-        data->data = nil;
+        data->data = NULL;
         data->len = 0;
     }
 }
