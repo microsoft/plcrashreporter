53dacfa398fe00e835a6cb4614b108fd15fd4f74
JiglioNero <v-msuendukov@microsoft.com>
2024-04-15 13:12:26 +0200

    test

--- Scripts/install-dependencies.sh
+++ Scripts/install-dependencies.sh
@@ -2,18 +2,11 @@
 
 PROTOBUF_VERSION="26.1"
 PROTOBUF_PLATFORM="osx-universal_binary"
-URL=https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}.zip
+PROTOBUF_C_VERSION="1.5.0"
+PROTOBUF_C_URL="https://github.com/protobuf-c/protobuf-c/releases/download/v${PROTOBUF_C_VERSION}/protobuf-c-${PROTOBUF_C_VERSION}.tar.gz"
+PROTOBUF_URL=https://github.com/google/protobuf/releases/download/v${PROTOBUF_VERSION}/protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}.zip
 
 brew install automake libtool wget
-echo "Removing old protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}"
-rm -rf protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}
-rm -rf protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}.zip
-
-echo "Downloading ${URL}"
-wget ${URL}
-
-echo "Unzipping protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}.zip"
-unzip protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}.zip -d protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}
 
 # Determine the shell
 if [[ $SHELL == */bash ]]; then
@@ -25,8 +18,38 @@ else
     exit 1
 fi
 
+echo "Config file: $CONFIG_FILE"
+
+# Install protoc
+echo "Removing old protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}"
+rm -rf protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}
+rm -rf protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}.zip
+
+echo "Downloading ${URL}"
+wget ${PROTOBUF_URL}
+
+echo "Unzipping protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}.zip"
+unzip protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}.zip -d protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}
+
 # Add protoc to PATH if not already present
-if [[ ":$PATH:" != *":${PWD}/protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}/bin:"* ]]; then
+if [[ ":$PзATH:" != *":${PWD}/protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}/bin:"* ]]; then
     echo "Adding protoc to PATH"
-    echo -e "export PATH=\"\$PATH:${PWD}/protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}/bin\"" >> "$CONFIG_FILE"
+    echo -e "\nexport PATH=\"${PWD}/protoc-${PROTOBUF_VERSION}-${PROTOBUF_PLATFORM}/bin:\$PATH\"" >> "$CONFIG_FILE"
+    source "$CONFIG_FILE"
+    echo protoc version: $(protoc --version)
 fi
+
+# Install protobuf-c
+echo "Removing old protobuf-c-${PROTOBUF_C_VERSION}"
+rm -rf protobuf-c-${PROTOBUF_C_VERSION}
+rm -rf protobuf-c-${PROTOBUF_C_VERSION}.tar.gz
+
+echo "Downloading ${PROTOBUF_C_URL}"
+wget ${PROTOBUF_C_URL}
+tar -xzvf protobuf-c-${PROTOBUF_C_VERSION}.tar.gz 
+
+cd protobuf-c-${PROTOBUF_C_VERSION}
+./autogen.sh
+./configure
+make
+make install
